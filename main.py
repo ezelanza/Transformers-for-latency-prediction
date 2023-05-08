ee95d36bbb100d9fc25f41b0306226f44ee7f63f
Eze Lanza (EZ) <ezequiel.lanza@gmail.com>
2023-05-08 09:33:04 -0400

    COMMIT

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ args = dotdict()
 
 args.model = 'informer' # model of experiment, options: [informer, informerstack, informerlight(TBD)]
 
-args.data = 'train_norm' # data
-args.root_path = './Dataset/Train/' # root path of data file
-args.data_path = 'Train_robust_45.csv' # data file
+args.data = 'ETTm1' # data
+args.root_path = './Informer/ETDataset/ETT-small/' # root path of data file
+args.data_path = 'ETTm1.csv' # data file
 args.features = 'MS' # forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate
 args.target = 'p95 deploy/frontend' # target feature in S or MS task
 args.freq = 't' # freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h
 args.checkpoints = './informer_checkpoints' # location of model checkpoints
 
-args.seq_len = 400 # input sequence length of Informer encoder
+args.seq_len = 500 # input sequence length of Informer encoder
 args.label_len = 360 # start token length of Informer decoder
 args.pred_len = 36 # prediction sequence length
 # Informer decoder input: concat[start token series(label_len), zero padding series(pred_len)]
 
-args.enc_in = 45 # encoder input size
-args.dec_in = 45 # decoder input size
+args.enc_in = 7 # encoder input size
+args.dec_in = 7 # decoder input size
 args.c_out = 1 # output size
 args.factor = 5 # probsparse attn factor
 args.d_model = 512 # dimension of model
@@ -38,17 +38,17 @@ args.e_layers = 2 # num of encoder layers
 args.d_layers = 1 # num of decoder layers
 args.d_ff = 2048 # dimension of fcn in model
 args.dropout = 0.05 # dropout
-args.attn = 'prob' # attention used in encoder, options:[prob, full]
+args.attn = 'full' # attention used in encoder, options:[prob, full]
 args.embed = 'timeF' # time features encoding, options:[timeF, fixed, learned]
 args.activation = 'gelu' # activation
 args.distil = True # whether to use distilling in encoder
 args.output_attention = False # whether to output attention in ecoder
 args.mix = True
-args.padding = 0
+args.padding = 1
 args.scale = False
 
 args.batch_size = 32 
-args.learning_rate = 0.0001
+args.learning_rate = 0.01
 args.loss = 'mse'
 args.lradj = 'type1'
 args.use_amp = False # whether to use automatic mixed precision training
---
+++ main_st.py
@@ -0,0 +1,1 @@
+ linear ettm1 --context_points 288 --target_points 96 --run_name linear_ettm1_regression --gpus 0 --use_seasonal_decomp --linear_window 288 --data_path /path/to/ETTm1.csv
